6c92ec3b2bd8993e1602d78755c70140c7753f49
9 Bit <pablo.pellegrinet@gmail.com>
2021-11-17 11:47:43 -0300

    Update ExcelExport.js
    excel de organizaciones ok

--- front/src/components/backoffice/admin/organizacion/ExcelExport.js
+++ front/src/components/backoffice/admin/organizacion/ExcelExport.js
@@ -4,34 +4,25 @@ const ExcelExport = ({ data }) => {
   const ExcelFile = ReactExport.ExcelFile;
   const ExcelSheet = ReactExport.ExcelFile.ExcelSheet;
   const ExcelColumn = ReactExport.ExcelFile.ExcelColumn;
-  /* 
-  nombre: "",
-    apellido: "",
-    cuil: "",
-    matricula: "",
-    email: "",
-    estado: "",
-    alias: "",
-    telefono: "",
-    tipo: "", */
+  
 
   if (data.length === 0) return <></>;
 
   return (
     <div className="row" style={{width:"100px"}}>
       <ExcelFile
-        filename="Usuarios"
+        filename="Organizaciones"
         element={
           <button className="btn btn-block btn-success">Download Excel</button>
         }
       >
-        <ExcelSheet data={data} name="Usuarios">
+        <ExcelSheet data={data} name="Organizaciones">
           <ExcelColumn label="Nombre" value="nombre" />
-          <ExcelColumn label="Apellido" value="apellido" />
-          <ExcelColumn label="Cuil" value="cuil" />
-          <ExcelColumn label="Alias" value="alias" />
+          <ExcelColumn label="Razon Social" value="razon_social" />
+          <ExcelColumn label="Ubicacion" value="ubicacion" />
           <ExcelColumn label="Email" value="email" />
           <ExcelColumn label="Telefono" value="telefono" />
+          <ExcelColumn label="Estado" value="estado" />
         </ExcelSheet>
       </ExcelFile>
     </div>
